289bdd21a4e4decf9973e9c79a5d93f590d8c81d
HT-Tuan <huynhthanhtuan2206@gmail.com>
2024-07-11 00:11:11 +0700

    feat: create converter time

--- movie/src/main/java/com/rental/movie/config/DatabaseConfig.java
+++ movie/src/main/java/com/rental/movie/config/DatabaseConfig.java
@@ -1,11 +1,18 @@
 package com.rental.movie.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.rental.movie.util.ZonedDateTimeReadConverter;
+import com.rental.movie.util.ZonedDateTimeWriteConverter;
 
 @Configuration
 public class DatabaseConfig extends AbstractMongoClientConfiguration {
@@ -18,8 +25,17 @@ public class DatabaseConfig extends AbstractMongoClientConfiguration {
     protected String getDatabaseName() {
         return database;
     }
+
     @Override
     public MongoClient mongoClient() {
         return MongoClients.create(uri);
+    }
+
+    @Override
+    public MongoCustomConversions customConversions() {
+        List<Converter<?, ?>> converters = new ArrayList<>();
+        converters.add(new ZonedDateTimeReadConverter());
+        converters.add(new ZonedDateTimeWriteConverter());
+        return new MongoCustomConversions(converters);
     }
 }
