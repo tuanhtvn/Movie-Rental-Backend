aba655f3be22567181509ff8a1b55170965e5d10
HT-Tuan <huynhthanhtuan2206@gmail.com>
2024-07-11 22:29:34 +0700

    feat: create config open api

---
+++ movie/src/main/java/com/rental/movie/config/OpenApiConfig.java
@@ -0,0 +1,34 @@
+package com.rental.movie.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class OpenApiConfig {
+    @Value("${open.api.title}")
+    private String title;
+    @Value("${open.api.version}")
+    private String version;
+    @Value("${open.api.description}")
+    private String description;
+    @Value("${open.api.local-server-url}")
+    private String localServerUrl;
+    @Value("${open.api.host-server-url}")
+    private String hostServerUrl;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title(this.title)
+                .version(this.version)
+                .description(this.description))
+                .servers(List.of(new Server().url(this.localServerUrl).description("Local server"),
+                        new Server().url(this.hostServerUrl).description("Host server")));
+    }
+}
