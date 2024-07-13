71eec7af430cde09f7b710c525074f1c59560be8
HT-Tuan <huynhthanhtuan2206@gmail.com>
2024-07-13 23:54:43 +0700

    feat: add authentication open api

--- movie/src/main/java/com/rental/movie/config/OpenApiConfig.java
+++ movie/src/main/java/com/rental/movie/config/OpenApiConfig.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
@@ -21,6 +24,13 @@ public class OpenApiConfig {
                 .version(this.appConfig.getVersion())
                 .description(this.appConfig.getDescription()))
                 .servers(List.of(new Server().url(this.appConfig.getHostServerUrl()).description("Host server"),
-                        new Server().url(this.appConfig.getLocalServerUrl()).description("Local server")));
+                        new Server().url(this.appConfig.getLocalServerUrl()).description("Local server")))
+                .components(
+                        new Components()
+                                .addSecuritySchemes("cookie", new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("cookie")))
+                .security(List.of(new SecurityRequirement().addList("cookie")));
     }
 }
