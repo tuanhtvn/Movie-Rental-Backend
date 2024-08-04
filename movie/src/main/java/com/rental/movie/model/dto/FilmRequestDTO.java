a891c80c4137c3dd36c425d78f9fa0be87660070
Tester <145645291+VTNaad@users.noreply.github.com>
2024-08-04 23:06:52 +0700

    update FilmController

--- movie/src/main/java/com/rental/movie/model/dto/FilmRequestDTO.java
+++ movie/src/main/java/com/rental/movie/model/dto/FilmRequestDTO.java
@@ -3,31 +3,84 @@ package com.rental.movie.model.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 public class FilmRequestDTO {
     @NotBlank(message = "Vui lòng nhập tên phim")
     @Size(max = 255, message = "Tên phim không được vượt quá 255 ký tự")
-    @Schema(description = "Tên phim", example = "Bố Già")
+    @Schema(description = "Tên phim", example = "Bố Già")
     @JsonProperty("FilmName")
     private String filmName;
-    @NotBlank(message = "Vui lòng chọn phim")
-    @Schema(description = "Đường dẫn của phim", example = "https://drive.google.com/file/d/1AUhDl9Mqfab3qL89-iOmoTKa-Z7kYHXU/view?usp=sharing")
-    @JsonProperty("FilmUrl")
-    private String filmlUrl;
-    @NotBlank(message = "Vui lòng chọn ảnh Thumbnail")
+
+    @NotBlank(message = "Vui lòng nhập mô tả phim")
+    @Schema(description = "Mô tả phim", example = "BỐ GIÀ là một bộ phim Web drama tình cảm gia đình, một dự án phim hài Tết 2020 của Trấn Thành.")
+    @JsonProperty("Description")
+    private String description;
+
+    @NotBlank(message = "Vui lòng chọn ảnh Thumbnail")
     @Schema(description = "Đường dẫn ảnh Thumbnail", example = "https://example.com/thumbnail.jpg")
     @JsonProperty("ThumbnailUrl")
     private String thumbnailUrl;
-    @NotBlank(message = "Vui lòng nhập mô tả phim")
-    @Schema(description = "Mô tả phim", example = "BỐ GIÀ là một bộ phim Web drama tình cảm gia đình, một dự án phim hài Tết 2020 của Trấn Thành. " +
-            "Trong phim, Trấn Thành đóng vai chính - một ông bố tính cục súc, bảo thủ nhưng rất thương con, luôn quan tâm gia đình. " +
-            "Phim xoay quanh đề tài thế giới giang hồ, xoáy vào chuyện giữ bản chất lương thiện hay chạy theo tiền bạc.")
-    @JsonProperty("Description")
-    private String description;
+
+    @Schema(description = "Đường dẫn Trailer", example = "https://example.com/trailer.mp4")
+    @JsonProperty("TrailerUrl")
+    private String trailerUrl;
+
+    @Schema(description = "Ngày phát hành", example = "2023-01-01")
+    @JsonProperty("ReleaseDate")
+    private ZonedDateTime releaseDate;
+
+    @Schema(description = "Thời lượng phim", example = "120")
+    @JsonProperty("Duration")
+    private String duration;
+
+    @Schema(description = "Diễn viên", example = "Trấn Thành, Lê Giang")
+    @JsonProperty("Actors")
+    private String actors;
+
+    @Schema(description = "Đạo diễn", example = "Trấn Thành")
+    @JsonProperty("Director")
+    private String director;
+
+    @Schema(description = "Ngôn ngữ", example = "Vietnamese")
+    @JsonProperty("Language")
+    private String language;
+
+    @Schema(description = "Số lượt xem", example = "100000")
+    @JsonProperty("NumberOfViews")
+    private Integer numberOfViews;
+
+    @Schema(description = "Đánh giá", example = "4.5")
+    @JsonProperty("Rating")
+    private Double rating;
+
+    @Schema(description = "Độ tuổi", example = "13")
+    @JsonProperty("Age")
+    private Integer age;
+
+    @Schema(description = "Giá", example = "20000")
+    @JsonProperty("Price")
+    private Double price;
+
+    @Schema(description = "Thời hạn giới hạn", example = "1000")
+    @JsonProperty("LimitTime")
+    private Integer limitTime;
+
+    private List<String> subtitleId = new ArrayList<>();
+    private List<String> narrationId = new ArrayList<>();
+    private List<String> commentId = new ArrayList<>();
 }
--- movie/src/main/java/com/rental/movie/model/dto/FilmResponseDTO.java
+++ movie/src/main/java/com/rental/movie/model/dto/FilmResponseDTO.java
@@ -3,21 +3,56 @@ package com.rental.movie.model.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
 public class FilmResponseDTO {
     @JsonProperty("Id")
     private String id;
     @JsonProperty("FilmName")
     private String filmName;
-    @JsonProperty("FilmlUrl")
-    private String filmUrl;
-    @JsonProperty("ThumbnailUrl")
-    private String thumbnailUrl;
     @JsonProperty("Description")
     private String description;
+    @JsonProperty("ThumbnailUrl")
+    private String thumbnailUrl;
+    @JsonProperty("TrailerUrl")
+    private String trailerUrl;
+    @JsonProperty("ReleaseDate")
+    private ZonedDateTime releaseDate;
+    @JsonProperty("Duration")
+    private String duration;
+    @JsonProperty("Actors")
+    private String actors;
+    @JsonProperty("Director")
+    private String director;
+    @JsonProperty("Language")
+    private String language;
+    @JsonProperty("NumberOfViews")
+    private Integer numberOfViews;
     @JsonProperty("Rating")
     private Double rating;
+    @JsonProperty("Age")
+    private Integer age;
+    @JsonProperty("Price")
+    private Double price;
+    @JsonProperty("LimitTime")
+    private Integer limitTime;
+    @JsonProperty("Subtitles")
+    private List<SubtitleResponseDTO> subtitleId = new ArrayList<>();
+    @JsonProperty("Narration")
+    private List<NarrationResponseDTO> narrationId = new ArrayList<>();
+    @JsonProperty("Comment")
+    private List<CommentResponseDTO> commentId = new ArrayList<>();
 }
