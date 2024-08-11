7cffe7cb567096e5fc414461b0f5b0aa511aef56
Tester <145645291+VTNaad@users.noreply.github.com>
2024-08-11 15:53:24 +0700

    update

--- movie/src/main/java/com/rental/movie/service/FilmServiceImpl.java
+++ movie/src/main/java/com/rental/movie/service/FilmServiceImpl.java
@@ -152,7 +152,7 @@ public class FilmServiceImpl implements FilmService {
     }
 
     @Override
-    public List<FilmResponseDTO> searchFilmByName(String keywords) {
+    public List<Film> searchFilmByName(String keywords) {
         String keyword = removeAccents(keywords).toLowerCase();
 
         return filmRepository.findByActived().stream()
